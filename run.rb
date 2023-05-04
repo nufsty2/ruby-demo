aa4b0711bbf072bc4209a36de475df81ad12a756
Jeff DeGraw <jeff.degraw@liveviewtech.com>
2023-05-04 13:28:38 -0600

    Initial commit

---
+++ run.rb
@@ -0,0 +1,19 @@
+puts "Can you play with me?"
+dads_answer = gets.chomp
+
+if dads_answer == "yes"
+  puts "\nlet's play!"
+elsif dads_answer == "no"
+  puts "\nok dad"
+end
+
+puts "Can you come out?"
+dads_answer = gets.chomp
+if dads_answer == "yes"
+  puts "\nok"
+elsif dads_answer == "black"
+  puts "\nthat's my favorite color!"
+end
+
+puts "\ni love you!"
+# puts "i can read"
